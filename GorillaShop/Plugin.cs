194d8378bdbf33e2bc7562a80b510997cd3b9adb
Graze <me@graze.cc>
2026-02-18 09:01:48 +0000

    man

--- GorillaShop/Plugin.cs
+++ GorillaShop/Plugin.cs
@@ -2,28 +2,28 @@
 using GorillaNetworking;
 using System.IO;
 using System.Reflection;
+using BepInEx.Logging;
 using UnityEngine;
 namespace GorillaShop
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public static Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("GorillaShop.Assets.shop");
-        public static AssetBundle bundle = AssetBundle.LoadFromStream(str);
-        public static GameObject temp = bundle.LoadAsset<GameObject>("shopanch");
-        public static Material mat = bundle.LoadAsset<Material>("button");
-        public static GorillaKeyboardButton ButtonBase;
+        private static readonly Stream Str = Assembly.GetExecutingAssembly().GetManifestResourceStream("GorillaShop.Assets.shop");
+        private static readonly AssetBundle Bundle = AssetBundle.LoadFromStream(Str);
+        public static readonly GameObject Temp = Bundle.LoadAsset<GameObject>("shopanch");
+        public static readonly Material Mat = Bundle.LoadAsset<Material>("button");
 
-        Plugin() => HarmonyPatches.ApplyHarmonyPatches();
-        void Start()
+        private Plugin() => HarmonyPatches.ApplyHarmonyPatches();
+        public static ManualLogSource PluginLogs;
+        private void Start()
         {
             GorillaTagger.OnPlayerSpawned(delegate
             {
-                new GameObject("GorillaShop Manager", typeof(ShopManager));
-                ButtonBase = FindObjectOfType<GorillaKeyboardButton>();
-                str.Close();
-                bundle.UnloadAsync(false);
-
+                PluginLogs = Logger;
+                var o = new GameObject("GorillaShop Manager", typeof(ShopManager));
+                Str.Close();
+                Bundle.UnloadAsync(false);
                 DontDestroyOnLoad(new GameObject("KeyboardListener", typeof(KeyboardListner)));
             });
         }
--- GorillaShop/ShopManager.cs
+++ GorillaShop/ShopManager.cs
@@ -1,277 +1,231 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using GorillaNetworking;
-using Photon.Pun;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using System.Collections;
 
 namespace GorillaShop
 {
     public class ShopManager : MonoBehaviour
     {
-        public static ShopManager Instance { get; private set; }
+        public static ShopManager Instance;
 
-        public List<CosmeticsController.CosmeticItem> AllItems => CosmeticsController.instance.allCosmetics;
-        public List<CosmeticsController.CosmeticItem> AvailableItems { get; private set; }
-        public List<CosmeticsController.CosmeticItem> SetItems = new List<CosmeticsController.CosmeticItem>();
+        private static List<CosmeticsController.CosmeticItem> AllItems =>
+            CosmeticsController.instance.allCosmetics;
 
-        private Canvas canvas;
-        public GameObject ItemPrefab, StuffContainer;
+        private List<CosmeticsController.CosmeticItem> _allItems;
+        private List<string> _setItems = [];
+        private List<CosmeticsController.CosmeticItem> _visibleItems;
 
-        private bool reset, firstRun;
+        private static readonly Material SpriteMat = new Material(Shader.Find("UI/Default"));
+        private static readonly HashSet<Texture> FilteredTextures = new();
 
-        public GorillaPressableButton ButtonBase;
-        private ScollBar upScroll, downScroll;
+        private Vector2 _scrollPos;
+        private Rect _windowRect;
 
-        private Vector3 citySpot = new Vector3(-49.9966f, 16.826f, -117.5916f);
-        void Start()
-        { 
-            CosmeticsV2Spawner_Dirty.OnPostInstantiateAllPrefabs2 += InitializeShop;
-        }
-        void InitializeShop()
+        private bool _show;
+        private bool _initialized;
+
+        private string _search = "";
+        private string _lastSearch = "";
+
+        private const float RowHeight = 90f;
+        private const float IconSize = 64f; 
+
+        private void Awake()
         {
             Instance = this;
-            transform.SetParent(GameObject.Find("Environment Objects/LocalObjects_Prefab/City_WorkingPrefab").transform, true);
-            canvas = Instantiate(Plugin.temp, transform).transform.GetChild(0).GetComponent<Canvas>();
-            Destroy(Plugin.temp);
-            canvas.renderMode = RenderMode.WorldSpace;
-            canvas.transform.parent.name = "GorillaShop";
-            canvas.targetDisplay = 0;
-            canvas.scaleFactor = 1.5f;
+            
+            var windowWidth = Mathf.Min(750, Screen.width * 0.5f);
+            var windowHeight = Mathf.Min(750, Screen.height * 0.75f);
+            _windowRect = new Rect(Screen.width * 0.25f, Screen.height * 0.1f, windowWidth, windowHeight);
 
-            StuffContainer = canvas.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
-            ItemPrefab = StuffContainer.transform.GetChild(0).gameObject;
-
-            Transform baseTransform = transform.GetChild(0);
-            baseTransform.localPosition = Vector3.zero;
-            baseTransform.GetChild(0).localPosition = Vector3.zero;
-            baseTransform.GetChild(0).GetChild(0).localPosition = Vector3.zero;
-
-            transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-            transform.position = new Vector3(-52.7105f, 16.7912f, -121.9634f);
-            transform.localRotation = Quaternion.Euler(0, 321, 0);
-            StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
+            StartCoroutine(WaitForCosmeticsReady());
         }
 
-        void OnEnable()
+        private IEnumerator WaitForCosmeticsReady()
         {
-            if (firstRun)
-            {
-                return;
-            }
-            else if (CosmeticsController.instance.allCosmeticsItemIDsfromDisplayNamesDict_isInitialized)
-            {
-                InitializeScrollBars();
-                StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
-                MakeToggle();
-                PopulateShopItems();
-                UnlockFreeItems();
-                firstRun = true;
-            }
+            while (!CosmeticsV2Spawner_Dirty.allPartsInstantiated)
+                yield return null;
 
+            while (!CosmeticsController.instance ||
+                   CosmeticsController.instance.allCosmetics == null ||
+                   CosmeticsController.instance.allCosmetics.Count == 0)
+                yield return null;
+            
+            InitializeItems();
+            
+            _initialized = true;
+        }
+        
+        private void InitializeItems()
+        {
+            _allItems = AllItems
+                .Where(x => x.canTryOn)
+                .ToList();
+            
+            _allItems.RemoveAll(item => CosmeticsController.instance.unlockedCosmetics.Contains(item));
+            
+            var setItems = new HashSet<string>(
+                _allItems
+                    .Where(item => item.itemCategory == CosmeticsController.CosmeticCategory.Set)
+                    .SelectMany(item => item.bundledItems)
+                    .Concat(
+                        CosmeticsController.instance.bundleList.data
+                            .SelectMany(bundle =>
+                                _allItems
+                                    .Where(item => item.itemName == bundle.playFabItemName)
+                                    .SelectMany(item => item.bundledItems))
+                    )
+            );
+            
+            _allItems.RemoveAll(item => setItems.Contains(item.itemName));
+            
+            UnlockFreeItems(_allItems);
+            _allItems.RemoveAll(item => item.cost == 0); // that stupid DJ set items still kept showing up :(
+            _visibleItems = new List<CosmeticsController.CosmeticItem>(_allItems);
         }
 
-        void UnlockFreeItems()
+
+        private static void UnlockFreeItems(List<CosmeticsController.CosmeticItem> list)
         {
-            foreach (var item in AllItems)
+            foreach (var item in list
+                         .Where(i => i is { canTryOn: true, cost: 0 })
+                         .Where(i => !CosmeticsController.instance.unlockedCosmetics.Contains(i)))
             {
-                if (item.canTryOn && item.cost == 0 && !CosmeticsController.instance.unlockedCosmetics.Contains(item))
-                {
-                    CosmeticsController.instance.itemToBuy = item;
-                    CosmeticsController.instance.PurchaseItem();
-                    StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
-                }
+                CosmeticsController.instance.itemToBuy = item;
+                CosmeticsController.instance.PurchaseItem();
             }
         }
 
-        void PopulateShopItems()
+        private void OnGUI()
         {
-            AvailableItems = AllItems.Where(item => item.canTryOn).ToList();
+            if (!_initialized) return;
+            
+            const float buttonWidth = 150f;
+            const float buttonHeight = 40f;
 
-            foreach (var item in AvailableItems)
+            if (GUI.Button(new Rect(20, 20, buttonWidth, buttonHeight), "SHOP"))
+                _show = !_show;
+
+            if (!_show) return;
+
+            _windowRect = GUI.Window(888, _windowRect, DrawWindow, "Gorilla Shop");
+        }
+
+        private void DrawWindow(int id)
+        {
+            DrawSearchBar();
+
+            var scrollWidth = _windowRect.width - 20;
+            var scrollHeight = _windowRect.height - 90;
+            var scrollRect = new Rect(10, 60, scrollWidth, scrollHeight);
+            var contentRect = new Rect(0, 0, scrollWidth - 20, _visibleItems.Count * RowHeight);
+
+            _scrollPos = GUI.BeginScrollView(scrollRect, _scrollPos, contentRect);
+
+            var firstVisible = Mathf.FloorToInt(_scrollPos.y / RowHeight);
+            var visibleCount = Mathf.CeilToInt(scrollHeight / RowHeight) + 2;
+            var lastVisible = Mathf.Min(firstVisible + visibleCount, _visibleItems.Count);
+
+            for (var i = firstVisible; i < lastVisible; i++)
+                DrawRow(_visibleItems[i], i);
+
+            GUI.EndScrollView();
+            GUI.DragWindow(new Rect(0, 0, 10000, 30));
+        }
+
+        private void DrawSearchBar()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Search:");
+            _search = GUILayout.TextField(_search);
+            GUILayout.EndHorizontal();
+
+            if (_search == _lastSearch) return;
+            ApplySearch();
+            _lastSearch = _search;
+        }
+
+        private void ApplySearch()
+        {
+            if (string.IsNullOrEmpty(_search))
             {
-                try
-                {
-
-                    if (item.itemCategory == CosmeticsController.CosmeticCategory.Set)
+                _visibleItems = new List<CosmeticsController.CosmeticItem>(_allItems);
+            }
+            else
+            {
+                var lower = _search.ToLowerInvariant();
+                _visibleItems = _allItems
+                    .Where(x =>
                     {
-                        if (item.bundledItems.Count() >= 1)
-                        {
-                            foreach (string s in item.bundledItems)
-                            {
-                                SetItems.Add(CosmeticsController.instance.GetItemFromDict(s));
-                            }
-                        }
-                    }
-                    var shopItem = Instantiate(ItemPrefab, StuffContainer.transform).AddComponent<ShopItem>();
-                    shopItem.Item = item;
-                    StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
-                }
-                catch
-                {
-
-                }
+                        string name = string.IsNullOrEmpty(x.overrideDisplayName)
+                            ? x.displayName
+                            : x.overrideDisplayName;
+                        return name != null && name.ToLowerInvariant().Contains(lower);
+                    })
+                    .ToList();
             }
-            StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
+            _scrollPos = Vector2.zero;
         }
 
-        void InitializeScrollBars()
+        private void DrawRow(CosmeticsController.CosmeticItem item, int index)
         {
-            if (upScroll == null)
+            var y = index * RowHeight;
+            var rowRect = new Rect(0, y, _windowRect.width - 40, RowHeight - 5);
+
+            GUI.Box(rowRect, GUIContent.none);
+
+            if (item.itemPicture)
             {
-                upScroll = MakeScroller(true);
-                upScroll.transform.localScale = new Vector3(30, 30, 30);
-                upScroll.transform.localPosition = new Vector3(20.771f, -18.5237f, 6.6018f);
-
-                downScroll = MakeScroller(false);
-                downScroll.transform.localScale = new Vector3(30, 30, 30);
-                downScroll.transform.localPosition = new Vector3(20.771f, 403.9127f, 6.6018f);
-                downScroll.transform.localRotation = Quaternion.Euler(0, 0, 90);
-
-
-                StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
-            }
-        }
-
-        void FixedUpdate()
-        {
-            if (ButtonBase == null)
-            {
-                ButtonBase = FindObjectOfType<GorillaPressableButton>();
-            }
-        }
-
-        ScollBar MakeScroller(bool isUp)
-        {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var meshFilter = cube.GetComponent<MeshFilter>();
-            var scollBar = Instantiate(ButtonBase, transform).AddComponent<ScollBar>();
-            scollBar.GetComponent<MeshFilter>().mesh = meshFilter.mesh;
-            scollBar.GetComponent<MeshRenderer>().material = Plugin.mat;
-            Destroy(scollBar.GetComponent<GorillaPressableButton>());
-            Destroy(cube);
-            scollBar.GetComponent<ScollBar>().up = isUp;
-            scollBar.name = "Scroll Button: Up = " + isUp;
-            return scollBar;
-        }
-
-        ToggleButton MakeToggle()
-        {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var meshFilter = cube.GetComponent<MeshFilter>();
-            var Show = Instantiate(ButtonBase, transform).AddComponent<ToggleButton>();
-            Show.GetComponent<MeshFilter>().mesh = meshFilter.mesh;
-            Show.GetComponent<MeshRenderer>().material = Plugin.mat;
-            Destroy(Show.GetComponent<GorillaPressableButton>());
-            Destroy(cube);
-            Show.transform.SetParent(transform);
-            Show.name = "ShowHide Button";
-            return Show;
-        }
-
-        public void Toggle(bool toggle)
-        {
-            upScroll.gameObject.SetActive(toggle);
-            downScroll.gameObject.SetActive(toggle);
-            transform.GetChild(0).gameObject.SetActive(toggle);
-        }
-
-        public class ShopItem : MonoBehaviour
-        {
-            public CosmeticsController.CosmeticItem Item { get; set; }
-            private Image image;
-            private TextMeshProUGUI displayName, price;
-            private GameObject button;
-            private TryButton tryButton;
-
-            void Awake()
-            {
-                image = transform.GetChild(0).GetComponent<Image>();
-                displayName = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-                price = transform.GetChild(3).GetComponent<TextMeshProUGUI>();
-                button = transform.GetChild(4).gameObject;
-                button.SetActive(true);
+                DrawSprite(new Rect(10, y + 13, IconSize, IconSize), item.itemPicture);
             }
 
-            void Start() => InitializeItem();
+            var displayName = string.IsNullOrEmpty(item.overrideDisplayName)
+                ? item.displayName
+                : item.overrideDisplayName;
 
-            void InitializeItem()
+            GUI.Label(new Rect(85, y + 15, 350, 25), displayName);
+            GUI.Label(new Rect(85, y + 40, 200, 25), "Price: " + item.cost);
+
+            var cart = CosmeticsController.instance.currentCart;
+            var inCart = cart.Contains(item);
+
+            GUI.backgroundColor = inCart ? Color.red : Color.green;
+
+            if (GUI.Button(new Rect(rowRect.width - 140, y + 25, 120, 30), inCart ? "Remove" : "Add"))
             {
-                if (displayName.text == "NOTHING" || ShopManager.Instance.SetItems.Contains(Item))
-                {
-                    Destroy(gameObject);
-                    return;
-                }
-                image.sprite = Item.itemPicture;
-                image.overrideSprite = Item.itemPicture;
+                if (inCart) cart.Remove(item);
+                else if (cart.Count < 12) cart.Add(item);
 
-                price.text = Item.cost.ToString();
-
-                displayName.text = !string.IsNullOrEmpty(Item.overrideDisplayName) ? Item.overrideDisplayName : Item.displayName;
-                gameObject.name = displayName.text;
-
-
-
-                tryButton = MakeButton();
-                tryButton.name = "CartButton";
-                tryButton.transform.localScale = new Vector3(108, 109, 1);
-                tryButton.transform.localPosition = new Vector3(2, 0, 0);
-                tryButton.Item = this;
-                if (CosmeticsController.instance.unlockedCosmetics.Contains(Item))
-                {
-                    Destroy(gameObject);
-                    Instance.StuffContainer.transform.localPosition = new Vector3(0, -999999, 0);
-                }
-                StartCoroutine(OwnedCheck());
-            }
-
-            IEnumerator OwnedCheck()
-            {
-                yield return new WaitForSeconds(10);
-                if (CosmeticsController.instance.unlockedCosmetics.Contains(Item))
-                {
-                    Destroy(gameObject);
-                    StopCoroutine(OwnedCheck());
-                }
-                yield return StartCoroutine(OwnedCheck());
-            }
-
-            public void ButtonPress()
-            {
-                var cart = CosmeticsController.instance.currentCart;
-
-                if (cart.Count >= 12)
-                {
-                    cart.Clear();
-                }
-                if (cart.Contains(Item))
-                {
-                    cart.Remove(Item);
-                }
-                else
-                {
-                    cart.Add(Item);
-                }
                 CosmeticsController.instance.UpdateShoppingCart();
             }
 
-            TryButton MakeButton()
+            GUI.backgroundColor = Color.white;
+        }
+
+        private static void DrawSprite(Rect rect, Sprite sprite)
+        {
+            if (!sprite || !sprite.texture) return;
+
+            var texture = sprite.texture;
+
+            if (!FilteredTextures.Contains(texture))
             {
-                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                var meshFilter = cube.GetComponent<MeshFilter>();
-                var butt = Instantiate(Instance.ButtonBase, button.transform).AddComponent<TryButton>();
-                butt.GetComponent<MeshFilter>().mesh = meshFilter.mesh;
-                Destroy(butt.GetComponent<WardrobeFunctionButton>());
-                Destroy(cube);
-                foreach (Transform t in butt.transform)
-                {
-                    Destroy(t.gameObject);
-                }
-                butt.GetComponent<Renderer>().enabled = false;
-                return butt;
+                texture.filterMode = FilterMode.Bilinear;
+                texture.wrapMode = TextureWrapMode.Clamp;
+                FilteredTextures.Add(texture);
             }
+
+            var tr = sprite.textureRect;
+
+            var uv = new Rect(
+                tr.x / texture.width,
+                tr.y / texture.height,
+                tr.width / texture.width,
+                tr.height / texture.height);
+
+            Graphics.DrawTexture(rect, texture, uv, 0, 0, 0, 0, Color.white, SpriteMat);
         }
     }
 }
